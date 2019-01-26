4672c51c5ef6994e1a1a7e1d760806e5400de529
Pragnesh Chaudhari <aarticianpc@gmail.com>
2019-01-26 18:54:34 +0530

    Prepared for production

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 // Getting authentication routes
 const auth = require('./routes/api/auth');
@@ -28,6 +29,15 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 app.use('/api/auth', auth);
+
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+}
 
 const port = process.env.PORT || 5000;
 
